a72150167bec477d4c4238ea7b56b5b1a6743040
LK <562586@student.fontys.nl>
2026-04-21 13:39:49 +0200

    secrets were wrong apparently, testing again
    april 21st 2026

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ function App() {
   return (
     <>
 
-      <h1>game tournament planner!</h1>
+      <h1>game tournament planner</h1>
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
