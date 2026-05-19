f7d9330a56b3facc2ae143cc1400e41c580bba1c
LK <562586@student.fontys.nl>
2026-05-19 12:55:58 +0200

    added .jsx for the routing, this should do the trick
    may 19th 2026

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 
-import Home from './pages/Home'
-import Register from './pages/Register'
+import Home from './pages/Home.jsx'
+import Register from './pages/Register.jsx'
 
 function App() {
   return (
