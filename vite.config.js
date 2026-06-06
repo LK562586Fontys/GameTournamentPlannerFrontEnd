75cbe4ed88d2997d81d88a220a35d02850e092d4
LK <562586@student.fontys.nl>
2026-06-06 10:49:05 +0200

    updated vite.config.js to include coverage

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov', 'html'],
+      reportsDirectory: 'coverage',
+    },
     globals: true,
     environment: 'jsdom',
   },
